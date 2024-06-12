f6147e48926ab777aea10c18be30a7eaa109344c
Stella Chinonso <starocani01@gmail.com>
2024-06-12 09:18:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
+
+  useEffect(function () {
+    async function convert() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+      );
+      const data = await res.json();
+      console.log(data.rates[toCur]);
+    }
+  }, []);
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onchange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select value={fromCur} onchange={(e) => setFromCur(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={toCur} onchange={(e) => setToCur(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
